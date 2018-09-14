563e675af2c957ba259f7ff930ae592f21982111
gerlik <gerliks@gmail.com>
2018-09-14 13:02:21 +0300

    Todolist started

--- myCtrl.js
+++ myCtrl.js
@@ -1,3 +1,27 @@
-angular.module("myApp", []).controller("myCtrl", function ($scope) {
-    
-});
+//TODO fix the design, add deletation and edit options, move done tasks elswhere, show percentage or 1/4 tasks done etc
+//We have to define our application first
+angular.module("todoApp", []).controller("todoController", ['$scope',
+                                 function($scope) {
+                                     //A model holding tasks
+                                     $scope.taskList = [
+                                         {done: true,
+                                          task: 'Do nothing'
+                                         },
+                                         {
+                                             done: false,
+                                             task: 'Show some tasks'
+                                         }
+                                     ];
+
+                                     //Function for adding task to the task list
+                                     $scope.addTask = function(task) {
+
+                                         //I'm pushing a new task to the task list
+                                         $scope.taskList.push({
+                                             done: false,
+                                             task: task
+                                         });
+                                     };
+
+                                 }
+                                ]);
