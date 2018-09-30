5396ec7f3067bc6d908b9694cd07061bca218285
gerlik <gerliks@gmail.com>
2018-09-30 17:22:09 +0300

    Examples page and other adjustments

--- myCtrl.js
+++ myCtrl.js
@@ -1,3 +1,4 @@
+/*TODO APP*/
 angular.module("myTodoApp", []).controller("myTodoCtrl", ['$scope',
 function ($scope) {
 
@@ -42,7 +43,7 @@ function ($scope) {
     }
 ]);
 
-
+/*RESOURCES*/
 angular.module("myApp", []).controller("myCtrl", function ($scope) {
     $scope.sources = [
         {
@@ -81,4 +82,65 @@ angular.module("myApp", []).controller("myCtrl", function ($scope) {
         }
     ]
 });
+
+/*EXAMPLES*/
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+var animate = angular.module('exampleApp', ['ngRoute', 'ngAnimate']);
+
+animate.config(function ($routeProvider) {
+    $routeProvider
+        .when('/example', {
+            templateUrl: "info.html",
+            controller: "exampleCtrl"
+        })
+        .when('/code', {
+            templateUrl: "info.html",
+            controller: "codeCtrl"
+        })
+});
+
+animate.controller("exampleCtrl", function ($scope, $route) {
+    $scope.example = [
+        {
+            url: 'expl1_2.PNG'
+        },
+        {
+            url: 'expl1_1.PNG'
+        }
+    ]
+});
+
+animate.controller("codeCtrl", function ($scope, $route) {
+    $scope.code =
+        '<!DOCTYPE html>\n' +
+        '<html>\n' +
+        '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>\n' +
+        '<body>\n' +
+        '\n' +
+        '<div ng-app="">\n' +
+        '\n' +
+        '<p>Input something in the input box:</p>\n' +
+        '<p>Name : <input type="text" ng-model="name" placeholder="Enter name here"></p>\n' +
+        '<h1>Hello {{name}}</h1>\n' +
+        '\n' +
+        '</div>\n' +
+        '' +
+        '</body>\n' +
+        '</html>';
+});
+
 
